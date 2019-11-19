6ebb22db5bda57b3b35804b3c0aa52163ccd9db8
justinus789 <54980265+justinus789@users.noreply.github.com>
2019-11-19 09:27:27 +0700

    mercy

---
+++ precache-manifest.6c08b4df4ac850a95ec987f4025a3560.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21c39412f904d6ca909",
+    "url": "/css/chunk-vendors.d198af7f.css"
+  },
+  {
+    "revision": "4dc89b910d6d281bf0cd14cea8d0140a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "602649c26e4216ce05c8",
+    "url": "/js/app.0a8da701.js"
+  },
+  {
+    "revision": "e21c39412f904d6ca909",
+    "url": "/js/chunk-vendors.8665a86f.js"
+  },
+  {
+    "revision": "bf087c3fd3bcf675cb9541cf21e3ea5a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
